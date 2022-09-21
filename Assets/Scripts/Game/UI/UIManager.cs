aaba50cfb7eaa68801ef20d9bdb20416593c9a14
Edward Healy <edward.healy@polymtl.ca>
2022-09-20 22:27:28 -0400

    figured out toggle groups

--- Assets/Scripts/Game/UI/UIManager.cs
+++ Assets/Scripts/Game/UI/UIManager.cs
@@ -2,18 +2,26 @@ using System;
 using System.Collections.Generic;
 using Investigators;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+using Image = UnityEngine.UI.Image;
+using Toggle = UnityEngine.UI.Toggle;
 
 namespace Game
 {
     public class UIManager : MonoBehaviour
     {
         [SerializeField] private GameManager game;
-        [SerializeField] private Button buttonPrefab;
-        [SerializeField] private Button portraitPrefab;
+        [SerializeField] private ToggleGroup actionGroup;
+        [SerializeField] private ToggleGroup portraitGroup;
+        [SerializeField] private Toggle actionPrefab;
+        [SerializeField] private Toggle portraitPrefab;
         public GameObject actionPanel;
-        public GameObject investigatorsPanel;
+        public GameObject investigatorsPanel; 
         
         // Start is called before the first frame update
         void Start()
@@ -24,15 +32,17 @@ namespace Game
         {
             foreach (var actionId in actionTriggers.Keys)
             {
-                var button = Instantiate(buttonPrefab, actionPanel.transform);
-                button.GetComponentInChildren<TMP_Text>().text = ActionUtils.EnumToString(actionId);
-                button.onClick.AddListener(() =>  actionTriggers[actionId].Invoke());
+                var toggle = Instantiate(actionPrefab, actionGroup.transform);
+                toggle.GetComponentInChildren<Text>().text = ActionUtils.EnumToString(actionId);
+                toggle.group = actionGroup;
+                toggle.onValueChanged.AddListener((value) => TriggerAction(actionTriggers, actionId, value));
             }
         }
 
-        public void FooOnClick()
+        public void TriggerAction(Dictionary<ActionID, Action> actionTriggers, ActionID actionId, bool value)
         {
-            Debug.Log("Ta-Da!");
+            if (!value) return;
+            actionTriggers[actionId].Invoke();
         }
 
         // Update is called once per frame
@@ -46,10 +56,17 @@ namespace Game
         {
             foreach (var investigator in investigators.Values)
             {
-                var button = Instantiate(portraitPrefab, investigatorsPanel.transform);
-                button.GetComponentInChildren<Image>().sprite = investigator.GetSprite();
-                button.onClick.AddListener(() => game.currentInvestigator = investigator);
+                var toggle = Instantiate(portraitPrefab, portraitGroup.transform);
+                toggle.GetComponentInChildren<Image>().sprite = investigator.GetSprite();
+                toggle.group = portraitGroup;
+                toggle.onValueChanged.AddListener((value) => TriggerInvestigatorChange(investigator, value));
             }
+        }
+
+        private void TriggerInvestigatorChange(Investigator investigator, bool value)
+        {
+            if (!value) return;
+            game.currentInvestigator = investigator;
         }
     }
 }
